8ec0cb633da18de03e7fc0c8c5b6972ce3e0bd06
Pragati1200 <pragatiagrawalxyz@gmail.com>
2023-08-14 18:58:02 +0100

    cpp file is added

---
+++ main.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <list>
+using namespace std;
+
+class graph ()
+{
+    public:
+   int v;
+   list <int> *a;
+   graph(int v)
+   {
+      this.v=v;
+      list <int> a=new list[v];
+   }
+   void put_edge(int x,int y)
+   {
+       a[x].push_back[w];
+   }
+    void bfs(s);
+
+}
+
+void graph ::bfs(s)
+{
+    bool *visited=new bool[v];
+    for (int i=0;i<v;i++)
+    visited[i]=FALSE;
+
+
+    list <int> que[v];
+
+    visited[s]=TRUE;
+    que.push_back(s);
+
+    list::iterator i;
+    while(que!=)
+
+}
+int main()
+{
+
+
+    return 0;
+}
